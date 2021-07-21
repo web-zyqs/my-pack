30488e5de5eb0d4894e93bc3e98c5e7e6c048c3f
akaziki <beself29@gmail.com>
2021-07-21 14:39:10 +0800

    修改一些命名

--- createGraph.js
+++ createGraph.js
@@ -1,17 +1,17 @@
 const path = require("path");
 
-const createAsset = require("./createAsset.js");
+const createModule = require("./createModule.js");
 
 //这个函数用于从入口文件开始分析，生成依赖图
 function createGraph(entry,rules) {
   // 从入口文件开始
-  const entryAsset = createAsset(entry,rules);
+  const entryModule = createModule(entry,rules);
 
   // 我们使用一个队列来依次分析模块，刚开始队列中只有一个模块
-  const queue = [entryAsset];
+  const modules = [entryModule];
 
   // 遍历队列中的模块，刚开始只有一个模块，随后会添加进新的模块，然后继续遍历，直到剩余为空
-  for (const asset of queue) {
+  for (const asset of modules) {
     // 模块的依赖
     const { dependencies } = asset;
 
@@ -27,17 +27,17 @@ function createGraph(entry,rules) {
       const absolutePath = path.join(dirname, relativePath);
 
       // 创建依赖的资源对象
-      const child = createAsset(absolutePath,rules);
+      const child = createModule(absolutePath,rules);
 
       // 将依赖对象的id添加到mapping对象上，方便后期查找
       asset.mapping[relativePath] = child.id;
 
       // 最后，将依赖资源对象加入到队列中。
-      queue.push(child);
+      modules.push(child);
     });
   }
 
-  return queue;
+  return modules;
 }
 
 module.exports = createGraph;
--- createModule.js
+++ createModule.js
@@ -7,7 +7,7 @@ const { transformFromAst } = require("@babel/core");
 let ID = 0;
 
 // 该函数以文件名（包含路径）为参数，返回资源对象
-function createAsset(filename, rules) {
+function createModule(filename, rules) {
   // 保存的依赖
   const dependencies = [];
 
@@ -21,15 +21,20 @@ function createAsset(filename, rules) {
     //如果不是js文件则使用loader中的规则转化成js，然后才能使用babel进行parse
     for (let i = 0; i < rules.length; i++) {
       let rule = rules[i];
+
       //使用配置里面的test正则匹配文件名
       if (rule.test.test(filename)) {
-        // 读取非js文件内容,判断loader是否导出了属性，如果导出了raw属性则将文件读取为Buffer
-        let loaderFunc = require(rule.use); //引入loader导出的函数
+
+        //引入loader
+        let loaderFunc = require(rule.use);
+
+        // 读取非js文件内容,判断loader是否导出了raw属性，如果导出了raw属性则将文件读取为Buffer,否则读取为utf-8字符串
         if (loaderFunc.raw) {
           content = fs.readFileSync(filename);
         } else {
           content = fs.readFileSync(filename, "utf-8");
         }
+
         content = loaderFunc(content); //执行loader导出的函数，并传入参数
       }
     }
@@ -59,4 +64,4 @@ function createAsset(filename, rules) {
   return { id, filename, code, dependencies };
 }
 
-module.exports = createAsset;
+module.exports = createModule;
